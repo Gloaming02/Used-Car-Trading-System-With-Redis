38cffe57a4c01f79dbae10079b6306aa81c484c1
Zhengyan Hu <91642985+Gloaming02@users.noreply.github.com>
2023-10-29 23:19:27 -0700

    Update SQLfile.sql

--- SQLfile.sql
+++ SQLfile.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS Customer;
 CREATE TABLE Customer (
     customer_id INTEGER PRIMARY KEY,
     username TEXT NOT NULL,
@@ -6,6 +7,7 @@ CREATE TABLE Customer (
     phone INTEGER NOT NULL
 );
 
+DROP TABLE IF EXISTS Save;
 CREATE TABLE Save (
     customer_id INTEGER NOT NULL,
     car_id INTEGER NOT NULL,
@@ -13,6 +15,7 @@ CREATE TABLE Save (
     FOREIGN KEY (car_id) REFERENCES Car(car_id)
 );
 
+DROP TABLE IF EXISTS Car;
 CREATE TABLE Car (
     car_id INTEGER PRIMARY KEY,
     make TEXT NOT NULL,
@@ -21,17 +24,24 @@ CREATE TABLE Car (
     price REAL NOT NULL,
     mileage TEXT NOT NULL,
     reportUrl TEXT NOT NULL,
-    location TEXT NOT NULL
+    location TEXT NOT NULL,
+    seller_id INTEGER NOT NULL,
+    FOREIGN KEY (seller_id) REFERENCES Seller(seller_id)
 );
 
+DROP TABLE IF EXISTS Appointment;
 CREATE TABLE Appointment (
     appointment_id INTEGER PRIMARY KEY,
-    year INTEGER NOT NULL,
-    month INTEGER NOT NULL,
-    day INTEGER NOT NULL,
-    clock INTEGER NOT NULL
+    date DATE NOT NULL,
+    customer_id INTEGER NOT NULL,
+    seller_id INTEGER NOT NULL,
+    car_id INTEGER NOT NULL,
+    FOREIGN KEY (customer_id) REFERENCES Customer(customer_id),
+    FOREIGN KEY (seller_id) REFERENCES Seller(seller_id),
+    FOREIGN KEY (car_id) REFERENCES Car(car_id)
 );
 
+DROP TABLE IF EXISTS Seller;
 CREATE TABLE Seller (
     seller_id INTEGER PRIMARY KEY,
     username TEXT NOT NULL,
